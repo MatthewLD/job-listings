0661ffea90ff1cea3f1f9248f79e4f90046a8caa
Máté Lanka <lankamate12@gmail.com>
2022-07-01 12:28:49 +0200

    Fixed some bugs

--- src/main/java/com/joblister/joblisterapp/controller/ClientRegistrationController.java
+++ src/main/java/com/joblister/joblisterapp/controller/ClientRegistrationController.java
@@ -2,8 +2,10 @@ package com.joblister.joblisterapp.controller;
 
 import com.joblister.joblisterapp.service.ClientRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -14,6 +16,7 @@ public class ClientRegistrationController {
     ClientRegistrationService clientRegistrationService;
 
     @PostMapping("/client")
+    @ResponseStatus(HttpStatus.CREATED)
     public Object registerClient(@RequestParam(value = "name") String name,
                                  @RequestParam(value = "email") String email) {
         return clientRegistrationService.registerClientInDB(name,email);
--- src/main/java/com/joblister/joblisterapp/controller/PositionSearchController.java
+++ src/main/java/com/joblister/joblisterapp/controller/PositionSearchController.java
@@ -3,10 +3,8 @@ package com.joblister.joblisterapp.controller;
 import com.joblister.joblisterapp.entity.Position;
 import com.joblister.joblisterapp.service.PositionSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,11 +15,13 @@ public class PositionSearchController {
     PositionSearchService positionSearchService;
 
     @GetMapping("/search")
+    @ResponseStatus(HttpStatus.OK)
     public Object searchPositions(@RequestParam(name = "Key word") String keyWord, @RequestParam(name = "Location") String location, @RequestParam(name = "API-key") String apiKey) {
         return positionSearchService.searchPosition(keyWord, location, apiKey);
     }
 
     @GetMapping("/search/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Position searchPositionById(@RequestParam(name = "id")Long id) {
         return positionSearchService.findById(id);
     };
